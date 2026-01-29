e7454385c2a8ef75846f2e67a7381ecf4c14f7a6
tmherfurth <tmherfurth1@my.scccd.edu>
2026-01-29 15:54:00 -0800

    Add files via upload

---
+++ VarsAndMath/src/VarsAndMath.java
@@ -0,0 +1,25 @@
+public class VarsAndMath {
+
+    public static void main(String[] args) {
+
+        // 1. Primitive-type variables
+        int a = 5;
+        int b = 6;
+        double c = 7;
+
+        // 2. Simple arithmetic
+        int sum = a + b;
+        int difference = a - b;
+        double scaled = c * 20;
+
+        // 3. String concatenation to display results
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+
+        System.out.println("The sum of a and b is " + sum);
+        System.out.println("The difference (a - b) is " + difference);
+        System.out.println("c multiplied by 20 is " + scaled);
+    }
+}
+
