365d772814b25d38ebc2bf95dd7825cfe1ec8098
tmherfurth <tmherfurth1@my.scccd.edu>
2026-01-29 19:27:58 -0800

    Add files via upload

---
+++ Greeting/src/SimpleGreeting.java
@@ -0,0 +1,8 @@
+void main() {
+    Scanner input = new Scanner(System.in);
+
+    IO.print("What is your name? ");
+    String name = input.nextLine();
+
+    IO.println("Hello, " + name + "! Nice to meet you.");
+}
